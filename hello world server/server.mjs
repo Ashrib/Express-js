e3f16315eb55d6f75015714159a8c4a9d45576c8
Syed Kariz Ali <karizali310@gmail.com>
2022-11-23 14:45:01 +0500

    first commit

---
+++ hello world server/server.mjs
@@ -0,0 +1,15 @@
+console.log("hi I'm server file");
+
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    console.log("request from ip:", req.ip);
+  res.send("Asharib's server!")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
